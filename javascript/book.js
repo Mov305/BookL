63f13913c2a1593e2648116c88e25b11f674d54e
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-08-01 17:33:59 -0300

    Add local storage function

--- javascript/book.js
+++ javascript/book.js
@@ -3,9 +3,14 @@ const bookTitle = document.querySelector('#bookTitle');
 const bookAuthor = document.querySelector('#bookAuthor');
 const addBook = document.querySelector('form > button[type=button]');
 
-let booksArray = [];
+function updateLocalStorage(data) {
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+let booksArray = JSON.parse(localStorage.getItem('data')) || [];
 
 const addUI = () => {
+  updateLocalStorage(booksArray);
   booklist.innerHTML = booksArray
     .map(
       (ele, id) => `<li>
@@ -19,6 +24,8 @@ const addUI = () => {
     )
     .join('');
 };
+
+addUI();
 
 /* eslint-disable */
 const removeUI = (id) => {
